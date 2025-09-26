fd9b86b8dc142a6ae2d9ebc4bc5132bc9c3c72f1
poopinmahbusiness <SANG043@e.ntu.edu.sg>
2025-09-26 14:37:37 -0400

    Added compression functionality and logging
    Changed the copying of files from uncompressed to compressed. Added logging via archive.log file, with timestamps

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # autoBASS
 An automated backup and sync system
+
+How to use:
+
+1. open up terminal(ubuntu)
+
+2. go to the correct directory
+
+3. do ./archive.sh [source directory] [destination directory]
+
+4. result.
+
+5. do ./archive.sh -h if you need help or instructions
--- archive.sh
+++ archive.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+# Script for MVP 
+# cd /mnt/c/Users/poopi/OneDrive\ -\ Nanyang\ Technological\ University/Semester\ 3.1\ Exchange/CSE\ 337\ Scripting\ Language/Assignment\ 1
+
+
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S") #Check the time
+echo "INFO: $TIMESTAMP archive script started" >> archive.log
 
 givehelp() {
     echo "Usage: $0 [source_directory] [target_directory]"
@@ -25,18 +30,36 @@ echo "Source Directory: $SOURCEDIRECTORY"
 echo "Target Directory: $TARGETDIRECTORY"
 
 if [ ! -d "$SOURCEDIRECTORY" ]; then
-    echo "Error: Source directory does not exist."
+    echo "Error: $TIMESTAMP Source directory does not exist or could not be created. Exiting" >> archive.log
     exit 1
 fi
 
 if [ ! -d "$TARGETDIRECTORY" ]; then
-    echo "Error: Target directory does not exist."
+    echo "Error: $TIMESTAMP Target directory does not exist or could not be created. Exiting" >> archive.log
     exit 1
 fi
 
-TIMESTAMP=$(date +"%Y%m%d_%H%M%S") #Check the time
-BACKUP_FOLDER="$TARGETDIRECTORY/backup_$TIMESTAMP" #Create a backup folder name
+
+
+echo "INFO: $TIMESTAMP Backing up from '$SOURCEDIRECTORY' to '$TARGETDIRECTORY'." >> archive.log
+
+BACKUP_FOLDER="$TARGETDIRECTORY/backup_$TIMESTAMP.tar.gz" #Create a backup folder name
 mkdir -p "$BACKUP_FOLDER" #Create the backup folder
 cp -r "$SOURCEDIRECTORY" "$BACKUP_FOLDER" #Copy the source directory to the backup folder
 
-echo "Backup of '$SOURCEDIRECTORY' completed successfully in '$BACKUP_FOLDER'."
+
+# Compress the backup folder into a .tar.gz file
+tar -czvf "$BACKUP_FOLDER.tar.gz" -C "$BACKUP_FOLDER" .
+
+# Check the exit status of the tar command
+if [ $? -ne 0 ]; then
+    echo "Error: $TIMESTAMP BACKUP FAILED DURING COMPRESSION." >> archive.log
+    exit 1
+fi
+
+#remove the uncompressed backup folder
+rm -rf "$BACKUP_FOLDER"
+
+
+echo "INFO: $TIMESTAMP Backup of '$SOURCEDIRECTORY' completed successfully in '$BACKUP_FOLDER'." >> archive.log
+exit 0
